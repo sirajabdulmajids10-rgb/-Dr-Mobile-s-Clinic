7cfeaae56b2a5d71c38cc540c1cd7e2ec5449b67
sirajabdulmajids10-rgb <sirajabdulmajid.s10@gmail.com>
2026-04-22 12:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 import os
-
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return "Website Working ✅"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
